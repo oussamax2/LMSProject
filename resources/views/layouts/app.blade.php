b71326ec8774397ff56130b9916f9dbcd6ee774d
ITmaxtn <itmax.tn@gmail.com>
2021-05-05 12:08:38 +0100

    update dachbord assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html >
 <head>
     <meta charset="UTF-8">
     <title>{{config('app.name')}}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 4.1.1 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
+    <link rel="stylesheet" href="{{ asset('assets-panel/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets-panel/css/bootstrap-datetimepicker.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.16/dist/css/coreui.min.css">
+    <link rel="stylesheet" href="{{ asset('assets-panel/css/coreui.min.css')}}">
+  
     <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@icon/coreui-icons-free@1.0.1-alpha.1/coreui-icons-free.css">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.3.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="{{ asset('assets-panel/css/coreui-icons-free.css')}}">
+    <link href="{{ asset('fonts/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('assets-panel/css/simple-line-icons.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets-panel/css/flag-icon.min.css')}}">
 </head>
 <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
 <header class="app-header navbar">
@@ -81,21 +82,18 @@
 <footer class="app-footer">
     <div>
         <a href="https://infyom.com">InfyOm </a>
-        <span>&copy; 2019 InfyOmLabs.</span>
+        <span>&copy; 2021 InfyOmLabs.</span>
     </div>
-    <div class="ml-auto">
-        <span>Powered by</span>
-        <a href="https://coreui.io">CoreUI</a>
-    </div>
+  
 </footer>
 </body>
 <!-- jQuery 3.1.1 -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@2.1.16/dist/js/coreui.min.js"></script>
+<script src="{{ asset('assets-panel/js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{ asset('assets-panel/js/popper.min.js')}}"></script>
+<script src="{{ asset('assets-panel/js/bootstrap.min.js')}}"></script>
+<script src="{{ asset('assets-panel/js/moment.min.js')}}"></script>
+<script src="{{ asset('assets-panel/js/bootstrap-datetimepicker.min.js')}}"></script>
+<script src="{{ asset('assets-panel/js/coreui.min.js')}}"></script>
 @stack('scripts')
 
 </html>
