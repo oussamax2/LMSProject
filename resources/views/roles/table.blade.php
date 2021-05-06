d65201f2ae1f38d6ce5134cd2785b1d0437ca6f3
ITmaxtn <itmax.tn@gmail.com>
2021-05-06 23:36:28 +0100

    add system role

---
+++ resources/views/roles/table.blade.php
@@ -0,0 +1,30 @@
+<div class="table-responsive-sm">
+    <table class="table table-striped" id="roles-table">
+        <thead>
+            <tr>
+                <th>Id</th>
+        <th>Name</th>
+        <th>Description</th>
+                <th colspan="3">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($roles as $role)
+            <tr>
+                <td>{{ $role->id }}</td>
+            <td>{{ $role->name }}</td>
+            <td>{{ $role->description }}</td>
+                <td>
+                    {!! Form::open(['route' => ['roles.destroy', $role->id], 'method' => 'delete']) !!}
+                    <div class='btn-group'>
+                        <a href="{{ route('roles.show', [$role->id]) }}" class='btn btn-ghost-success'><i class="fa fa-eye"></i></a>
+                        <a href="{{ route('roles.edit', [$role->id]) }}" class='btn btn-ghost-info'><i class="fa fa-edit"></i></a>
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-ghost-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
