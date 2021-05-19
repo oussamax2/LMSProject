bf7a262b652e3aff1885dcf99be9cf3ce5611943
DESKTOP-PUPD1VH\tunis <ho@tunisofts.com>
2021-05-19 16:03:24 +0100

    responsivve menu arabe and traduction RTL

--- resources/views/layouts/front/about-prof-tr.blade.php
+++ resources/views/layouts/front/about-prof-tr.blade.php
@@ -1,39 +1,37 @@
-			<!-- 
-			=============================================
-				Our Instructors Profile
-			============================================== 
-			-->
+{{-- information session --}}
 			<div class="our-instructor-profile">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-4 col-sm-6 col-xs-12"><img src="images/team/12.jpg" alt="">
                         <ul class="content-info-center">
-                            <li class="item-info">WebSite:</li>
-                            <li class="item-info">E-mail:</li>
-                            <li class="item-info">Telephone:</li>
+                            <li class="item-info">@lang('front.WebSite:')</li>
+                            <li class="item-info">@lang('front.E-mail:')</li>
+                            <li class="item-info">@lang('front.Telephone:')</li>
                         </ul>
                         </div>
 						<div class="col-md-8 col-xs-12">
 							<div class="text">
 								<div class="title">
-									<h4>Proffisional Training Center</h4>
-									<span>Senior Web Design &amp; Developer</span>
-								</div> <!-- /.title -->
+									<h4>@lang('front.Proffisional Training Center')</h4>
+									<span>@lang('front.Senior Web Design') &amp; @lang('front.Developer')</span>
+								</div>
 								<ul class="social-media">
 									<li><a href="" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
 									<li><a href="" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 									<li><a href="" class="tran3s"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
 									<li><a href="" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
 								</ul>
-								<p>Dalmatian hello amazing the rmore flung as thanks a manta dealt to under emu some the and one baldbe dear sobbingly save and spitefully less. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believabl.</p> <br>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu tellus ipsum. Nunc maximus sapien ac dui vulputate tincidunt. Morbi luctus nisi vel suscipit volutpat. Donec vitae auctor nisl. Ut malesuada felis in erat rutrum ultrices.</p>
+								<p>@lang('front.Dalmatian hello amazing the rmore flung as thanks a manta dealt to under emu some the and one baldbe dear sobbingly save and spitefully less. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believabl.')</p> <br>
+								<p>@lang('front.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu tellus ipsum. Nunc maximus sapien ac dui vulputate tincidunt. Morbi luctus nisi vel suscipit volutpat. Donec vitae auctor nisl. Ut malesuada felis in erat rutrum ultrices.')</p>
 								<ul class="instructor-achivement clearfix">
-									<li>Total Courses<h3>25,893</h3></li>
-									<li>Total Session<h3>79</h3></li>
-									<li>Total View<h3>1,698</h3></li>
+									<li>@lang('front.Total Courses')<h3>25,893</h3></li>
+									<li>@lang('front.Total Session')<h3>79</h3></li>
+									<li>@lang('front.Total View')<h3>1,698</h3></li>
 								</ul>
-							</div> <!-- /.text -->
-						</div> <!-- /.col- -->
-					</div> <!-- /.row -->
-				</div> <!-- /.container -->
-			</div> <!-- /.our-instructor-profile -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+{{-- information session --}}
