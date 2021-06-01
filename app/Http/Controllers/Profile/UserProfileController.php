bdb8b473efe97536ebd3c99670de24620ddb5947
TSP0045\Chedia Hoichtia <chediahoichtia1@gmail.com>
2021-06-01 09:53:38 +0100

    #12 Vendor (company)  update profile

--- app/Http/Controllers/Profile/UserProfileController.php
+++ app/Http/Controllers/Profile/UserProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
 
 class UserProfileController extends Controller
 {
@@ -18,6 +19,10 @@ class UserProfileController extends Controller
         return view('profile.edit')->with('user', $user);
 
     }
+    public function saveprofilepicture(UploadedFile $file) : string
+    {
+        return $file->store('profile_pictures', ['disk' => 'public']);
+    }
 
     function update(Request $request){
 
@@ -26,6 +31,7 @@ class UserProfileController extends Controller
         $this->validate($request, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email,'.$id,
+            'telephone' => 'required',
 
         ]);
 
@@ -37,7 +43,29 @@ class UserProfileController extends Controller
             $this->validate($request, ['password' => 'string|min:6|confirmed']);
     	    $user->password = bcrypt($request->password);
         }
+
+        $user->companies->telephone = $request->telephone;
+        $user->companies->website = $request->website;
+        $user->companies->shortDescription = $request->shortDescription;
+        $user->companies->description = $request->description;
+        $user->companies->fcburl = $request->fcburl;
+        $user->companies->twitturl = $request->twitturl;
+        $user->companies->linkdinurl = $request->linkdinurl;
+        $user->companies->dribbleurl = $request->dribbleurl;
+
     	$user->update();
+        $user->companies->update();
+        
+        if ($request->has('picture')){
+
+            /**save image in intended folder */
+            $image = $this->saveprofilepicture($request->file('picture'));
+            /**save image in database column */
+            $user->companies->picture = $image;
+            $user->companies->save();
+        }
+        
+
 
     	return redirect()->back();
     }
