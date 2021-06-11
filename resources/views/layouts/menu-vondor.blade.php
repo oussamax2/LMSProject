c778183b857b1dd039a79c07d057aa72c27a3821
TSP0045\Chedia Hoichtia <chediahoichtia1@gmail.com>
2021-06-11 15:48:15 +0100

    ***sidebar icons update

--- resources/views/layouts/menu-vondor.blade.php
+++ resources/views/layouts/menu-vondor.blade.php
@@ -2,19 +2,19 @@
 
 <li class="nav-item {{ Request::is('courses*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('courses.index') }}">
-        <span class="icon icon-docs"></span>
+        <span class="icon icon-graduation"></span>
         <span class="menu-item-dsh">@lang('front.Courses')</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('sessions*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('sessions.index') }}">
-        <span class="icon icon-briefcase"></span>
+        <span class="icon icon-calendar"></span>
         <span class="menu-item-dsh">@lang('front.Sessions')</span>
     </a>
 </li>
 <li class="nav-item {{ Request::is('registerations*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('registerations.index') }}">
-        <span class="icon icon-login"></span>
+        <span class="icon icon-user-following"></span>
         <span class="menu-item-dsh">@lang('front.Registerations')</span>
     </a>
 </li>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,19 +5,19 @@
         <span class="menu-item-dsh">@lang('front.Courses')</span><br/>
         <li class="nav-item {{ Request::is('admincourses*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('courses.index') }}">
-                <span class="icon icon-docs"></span>
+                <span class="icon icon-graduation"></span>
                 <span class="menu-item-dsh">@lang('front.Courses')</span>
             </a>
         </li>
         <li class="nav-item {{ Request::is('sessions*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('sessions.index') }}">
-                <span class="icon icon-briefcase"></span>
+                <span class="icon icon-calendar"></span>
                 <span class="menu-item-dsh">@lang('front.Sessions')</span>
             </a>
         </li>
         <li class="nav-item {{ Request::is('adminregisterations*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('registerations.index') }}">
-                <span class="icon icon-login"></span>
+                <span class="icon icon-user-following"></span>
                 <span class="menu-item-dsh">@lang('front.Registerations')</span>
             </a>
         </li>
@@ -27,19 +27,19 @@
 
         <li class="nav-item {{ Request::is('categories*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('categories.index') }}">
-                <span class="icon icon-wallet"></span>
+                <span class="icon icon-list"></span>
                 <span class="menu-item-dsh">@lang('front.Categories')</span>
             </a>
         </li>
         <li class="nav-item {{ Request::is('subcategories*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('subcategories.index') }}">
-                <span class="icon icon-wallet"></span>
+                <span class="icon icon-list"></span>
                 <span class="menu-item-dsh">@lang('front.Subcategories')</span>
             </a>
         </li>
         <li class="nav-item {{ Request::is('target_audiances*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('targetAudiances.index') }}">
-                <span class="icon icon-wallet"></span>
+                <span class="icon icon-target"></span>
                 <span class="menu-item-dsh">@lang('front.target_audiances')</span>
             </a>
         </li>
@@ -56,19 +56,19 @@
 
     <li class="nav-item {{ Request::is('users*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('users.index') }}">
-            <span class="icon icon-user"></span>
-            <span>@lang('front.Users')</span>
+            <span class="icon icon-people"></span>
+            <span class="menu-item-dsh">@lang('front.Users')</span>
         </a>
     </li>
     <li class="nav-item {{ Request::is('companies*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('companies.index') }}">
-        <span class="icon icon-drawer"></span>
+        <span class="icon icon-organization"></span>
         <span class="menu-item-dsh">@lang('front.Companies')</span>
     </a>
    </li>
    <li class="nav-item {{ Request::is('roles*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('roles.index') }}">
-        <span class="icon icon-graduation"></span>
+        <span class="icon icon-lock-open"></span>
         <span class="menu-item-dsh">@lang('front.Roles')</span>
     </a>
 </li>
@@ -107,14 +107,14 @@
 
 <li class="nav-item {{ Request::is('contacts*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('contacts.index') }}">
-        <span class="icon icon-envelope-open"></span>
+        <span class="icon icon-envelope-letter"></span>
         <span class="menu-item-dsh">@lang('front.Contacts')</span>
     </a>
 </li>
 
 <li class="nav-item {{ Request::is('languages*') ? 'active' : '' }}">
     <a class="nav-link" href="{{ route('languages.index') }}">
-    <span class="icon icon-globe"></span>
+    <span class="icon icon-flag"></span>
     <span class="menu-item-dsh">@lang('front.Languages')</span>
     </a>
 </li>
