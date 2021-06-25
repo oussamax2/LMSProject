e761ceafebbefd995f3e67b27a054f4de3c8d412
TSP0045\Chedia Hoichtia <chediahoichtia1@gmail.com>
2021-06-25 15:06:55 +0100

    *** #72 configure design mail validation

---
+++ resources/views/mail/validationmail.blade.php
@@ -0,0 +1,62 @@
+<h1><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    .information-user-company{
+        justify-content:center;
+        text-align:center;
+        align-items:center;
+        margin:auto;
+        background:#f3f3f3;
+        padding:25px;
+        box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 20%);
+        }
+        .information-user-company .content-mail-send h3{
+        font-size: 20px;
+        text-transform: uppercase;
+        font-weight: 500;
+        }
+        .information-user-company .content-mail-send p{
+            font-size: 15px;
+            font-weight: 400;
+        }
+        .information-user-company .content-mail-send a{
+            text-decoration: none;
+            color: #36d64c;
+            font-size: 15px;
+        }
+        .information-user-company .image-logolms img{
+            width:150px;
+        }
+    </style>
+</head>
+<body>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container">
+                <div class="information-user-company">
+                    <h6>Hello! </h6>
+                    <div class="image-logolms">
+                        <img src="https://i.postimg.cc/fLq6Rgmg/logo3.png" alt="">
+                    </div>
+                    <div class="content-mail-send">
+                    
+                    <h6>Click the button below to verify your email address. </h6>
+                    <a target="_blank" href="{{$url}}" class="button button-primary" style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; -webkit-text-size-adjust: none; border-radius: 4px; color: #fff; display: inline-block; overflow: hidden; text-decoration: none; background-color: #2d3748; border-bottom: 8px solid #2d3748; border-left: 18px solid #2d3748; border-right: 18px solid #2d3748; border-top: 8px solid #2d3748;">
+                        Verify Email Address
+                    </a>
+                    
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+</h1>
