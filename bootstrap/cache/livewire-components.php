33c431265d27c7367130f751e493d11780e56d75
oussama <oussamaouni.ing@gmail.com>
2021-09-02 15:42:47 +0100

    uapdate bootsrap

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'load-more-companies-data' => 'App\\Http\\Livewire\\LoadMoreCompaniesData',
+  'login' => 'App\\Http\\Livewire\\Login',
+  'registercompany' => 'App\\Http\\Livewire\\Registercompany',
+  'registeruser' => 'App\\Http\\Livewire\\Registeruser',
+  'search' => 'App\\Http\\Livewire\\Search',
+);
