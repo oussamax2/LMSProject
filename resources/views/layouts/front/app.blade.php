4ee2e095b445473da37fb93436ad31c53f45de57
oussama <OO@tunisofts.com>
2021-10-22 17:35:31 +0100

    2nd update logs

--- resources/views/layouts/front/app.blade.php
+++ resources/views/layouts/front/app.blade.php
@@ -5,11 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }}</title>
-    <title>Campus</title>
-    <meta name="description" content="Campus">
-    <meta name="tags" content="">
-    <meta name="author" content="Jeff Simons Decena">
+    <title>@yield('title')</title>
+    <meta property="og:url" content="{{ request()->url() }}"/>
+    @yield('og')
     @if(App::isLocale('en'))
     <link rel="icon" type="image/png" sizes="56x56" href="{{ asset('images/fav-icon/icon.png') }}">
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
@@ -25,8 +23,7 @@
     @endif
     <link rel="icon" type="image/png" sizes="56x56" href="{{ asset('images/fav-icon/icon.png') }}">
     @yield('css')
-    <meta property="og:url" content="{{ request()->url() }}"/>
-    @yield('og')
+
     @toastr_css
 </head>
 <body>
