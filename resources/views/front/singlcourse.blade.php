7f1e7898fcf0f852fed5cdbcbca5e4795332ff45
oussama <OO@tunisofts.com>
2021-12-08 14:32:43 +0100

    update meta

--- resources/views/front/singlcourse.blade.php
+++ resources/views/front/singlcourse.blade.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:title" content="{{ $sessions->courses->title }} - {{config('app.name')}}">
+    <meta property="og:title" content="{{ $sessions->courses->title }} - Corseat.com">
     <meta property="og:url" content="{{ request()->url() }}"/>
     <meta property="og:description" content="{!! \Illuminate\Support\Str::limit(strip_tags($sessions->courses->body), 160, $end='...') !!}">
     <meta property="og:image" content="{{ asset("storage/".$sessions->companies->picture) }}">
